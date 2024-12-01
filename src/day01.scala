3c7682ba13a52ae2dc66f689640b1f107581ab75
Joey Yu <joey@jyu.dev>
2024-12-01 11:56:52 -0500

    fix: bad solution

--- src/day01.scala
+++ src/day01.scala
@@ -2,7 +2,6 @@ package day01
 
 import locations.Directory.currentDir
 import inputs.Input.loadFileSync
-import scala.collection.mutable.PriorityQueue
 
 @main def part1: Unit =
   println(s"The solution is ${part1(loadInput())}")
@@ -13,28 +12,21 @@ import scala.collection.mutable.PriorityQueue
 def loadInput(): String = loadFileSync(s"$currentDir/../input/day01")
 
 def part1(input: String): String =
-  val list1 = PriorityQueue.empty[Int]
-  val list2 = PriorityQueue.empty[Int]
-
-  def insertNumber(line: String): Unit =
-    val firstDigit = line.find(_.isDigit).get.asDigit
-    val lastDigit = line.findLast(_.isDigit).get.asDigit
-    list1.enqueue(firstDigit)
-    list2.enqueue(lastDigit)
-    ()
-
-  input.linesIterator.foreach(insertNumber)
-
-  var total = 0
-
-  while (list1.nonEmpty && list2.nonEmpty) do
-    val first = list1.dequeue()
-    val last = list2.dequeue()
-
-    total += math.abs(first - last)
-
-  total.toString()
-
+  input.trim
+    .split("\n")
+    .map { line =>
+      line.trim
+        .split("\\s+")
+        .map(_.toInt)
+        .toList
+    }
+    .toList
+    .transpose
+    .map(_.sorted)
+    .transpose
+    .map { row => math.abs(row(0) - row(1)) }
+    .sum
+    .toString
 end part1
 
 def part2(input: String): String =

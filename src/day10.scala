bb52e86b9901967bd1bd51a05b3d4568ba974a01
Joey Yu <joey@jyu.dev>
2024-12-18 22:15:43 -0500

    chore: batch create all files

---
+++ src/day10.scala
@@ -0,0 +1,20 @@
+package day10
+
+import locations.Directory.currentDir
+import inputs.Input.loadFileSync
+
+@main def part1: Unit =
+  println(s"The solution is ${part1(loadInput())}")
+
+@main def part2: Unit =
+  println(s"The solution is ${part2(loadInput())}")
+
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day10")
+
+def part1(input: String): String =
+  ""
+end part1
+
+def part2(input: String): String =
+  ""
+end part2
---
+++ src/day14.scala
@@ -0,0 +1,20 @@
+package day14
+
+import locations.Directory.currentDir
+import inputs.Input.loadFileSync
+
+@main def part1: Unit =
+  println(s"The solution is ${part1(loadInput())}")
+
+@main def part2: Unit =
+  println(s"The solution is ${part2(loadInput())}")
+
+def loadInput(): String = loadFileSync(s"$currentDir/../input/day14")
+
+def part1(input: String): String =
+  ""
+end part1
+
+def part2(input: String): String =
+  ""
+end part2

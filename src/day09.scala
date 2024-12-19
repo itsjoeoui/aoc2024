3a3c2a15f5f3c62ce001b15852f1c33b16ec79dc
Joey Yu <joey@jyu.dev>
2024-12-19 09:50:52 -0500

    feat: day09 part1

--- src/day09.scala
+++ src/day09.scala
@@ -2,6 +2,7 @@ package day09
 
 import locations.Directory.currentDir
 import inputs.Input.loadFileSync
+import scala.annotation.tailrec
 
 @main def part1: Unit =
   println(s"The solution is ${part1(loadInput())}")
@@ -12,7 +13,42 @@ import inputs.Input.loadFileSync
 def loadInput(): String = loadFileSync(s"$currentDir/../input/day09")
 
 def part1(input: String): String =
-  ""
+  sealed trait Block
+  case class FileBlock(fileId: Int) extends Block
+  case object EmptyBlock extends Block
+
+  def checksum(system: Seq[Block]): Long =
+    system.zipWithIndex.foldLeft(0L) { case (acc, (block, index)) =>
+      block match
+        case FileBlock(fileId) =>
+          acc + fileId * index
+        case EmptyBlock =>
+          acc
+    }
+
+  def compact(system: Seq[Block]): Seq[Block] =
+    @tailrec
+    def rec(system: Seq[Block], acc: Seq[Block]): Seq[Block] =
+      if system.isEmpty then acc
+      else
+        system.head match
+          case EmptyBlock   => rec(system.last +: system.tail.init, acc)
+          case FileBlock(_) => rec(system.tail, acc :+ system.head)
+    rec(system, Seq.empty)
+
+  val system = input
+    .map(_.asDigit)
+    .grouped(2)
+    .zipWithIndex
+    .flatMap {
+      case (Seq(file, free), id) =>
+        List.fill(file)(FileBlock(id)) ++ List.fill(free)(EmptyBlock)
+      case (Seq(file), id) => List.fill(file)(FileBlock(id))
+    }
+    .toList
+
+  compact.andThen(checksum)(system).toString
+
 end part1
 
 def part2(input: String): String =
